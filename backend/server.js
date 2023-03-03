fd4b18a1f2e77d88264b55aad70708e7e3b3d7e1
Deva244 <49819715+Deva244@users.noreply.github.com>
2023-03-03 19:28:17 +0200

    updated routes and files for production

--- backend/server.js
+++ backend/server.js
@@ -24,10 +24,12 @@ app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
   //*Set static folder up in production
-  app.use(express.static("../frontend/build"));
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
   );
 }
 
